eb08f3bd0b3060ba9e701fcff5229f5efc9458d1
Viktor Avramovski <Viktor.Avramovski@students.finki.ukim.mk>
2020-06-27 15:21:04 +0200

    init

---
+++ book_catalog/src/test/java/mk/ukim/finki/emt/library/book_catalog/BookCatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package mk.ukim.finki.emt.library.book_catalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookCatalogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ orger_management/src/main/java/mk/ukim/finki/emt/library/orger_management/OrgerManagementApplication.java
@@ -0,0 +1,13 @@
+package mk.ukim.finki.emt.library.orger_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrgerManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrgerManagementApplication.class, args);
+    }
+
+}
