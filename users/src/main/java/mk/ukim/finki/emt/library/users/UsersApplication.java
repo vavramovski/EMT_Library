48b16224add6a02a0d335a122de92bbbaef4220c
jovanvelkovski <velkovski.jovan@yahoo.com>
2020-06-29 23:36:21 +0200

    Implemented users module

--- users/src/main/java/mk/ukim/finki/emt/library/users/UsersApplication.java
+++ users/src/main/java/mk/ukim/finki/emt/library/users/UsersApplication.java
@@ -1,9 +1,16 @@
 package mk.ukim.finki.emt.library.users;
 
+import mk.ukim.finki.emt.library.shared_kernel.SharedConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Import;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EntityScan
+@EnableJpaRepositories
+@Import(SharedConfiguration.class)
 public class UsersApplication {
 
     public static void main(String[] args) {
